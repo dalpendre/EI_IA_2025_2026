dc2e8fb5d51a27d3aed32cd79c34ddd57a5467d0
Diogo Alpendre <dalpendre1999@gmail.com>
2026-02-26 22:24:19 +0000

    Worksheet 1 - decide a to c

---
+++ 01_Worksheet/cell.py
@@ -0,0 +1,44 @@
+
+
+class Cell:
+
+    def __init__(self, line=0, column=0):
+        self.line = line
+        self.column = column
+        self.has_wall = False
+        self.dirty = False
+        self.agent = None
+        self.last_iteration = 0
+
+    @property
+    def color(self) -> str:
+        if self.has_agent():
+            return 'red'
+        if self.has_wall:
+            return 'blue'
+        if self.dirty:
+            return 'gray'
+        return 'white'
+
+    def set_last_iteration(self, last_iteration):
+        self.last_iteration = last_iteration
+
+    def has_agent(self) -> bool:
+        return self.agent is not None
+
+    def set_agent(self, agent) -> None:
+        self.agent = agent
+        if self.has_agent():
+            self.last_iteration = agent.iteration + 1
+            if self.dirty:
+                self.dirty = False
+
+    def __str__(self) -> str:
+        if self.has_wall:
+            return '#'
+        elif self.has_agent():
+            return 'X'
+        elif self.dirty:
+            return '-'
+        else:
+            '.'
---
+++ 01_Worksheet/perception.py
@@ -0,0 +1,12 @@
+
+from cell import Cell
+
+
+class Perception:
+
+    def __init__(self, n: Cell, s: Cell, e: Cell, w: Cell):
+        self.n = n
+        self.s = s
+        self.e = e
+        self.w = w
+
